46fb77d140aacb1f84cee5a5241f8a25ed593a93
rodrigosozup <rodrigo.deoliveira@zup.com.br>
2021-08-24 11:00:52 -0300

    Mudanca no status de resposta do controller de producer

--- producer_nats/src/main/kotlin/br/com/rodrigo/producer/controller/IngressoController.kt
+++ producer_nats/src/main/kotlin/br/com/rodrigo/producer/controller/IngressoController.kt
@@ -2,7 +2,6 @@ package br.com.zup.rodrigo.producer.controller
 
 import br.com.rodrigo.producer.service.IngressoService
 import br.com.zup.rodrigo.dto.IngressoRequest
-import br.com.zup.rodrigo.dto.IngressoResponse
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.*
 import io.micronaut.validation.Validated
@@ -15,21 +14,22 @@ class IngressoController(private val ingressoService: IngressoService) {
     @Post
     fun cadastrar(@Body @Valid ingressoRequest: IngressoRequest): HttpResponse<Any> {
         val ingresso = ingressoRequest.ToModel()
-        return HttpResponse.created(ingressoService.cadastrar(ingresso))
+        ingressoService.cadastrar(ingresso)
+        return HttpResponse.ok()
     }
 
     @Put("/{id}")
     fun atualizar(@PathVariable id: String, @Body @Valid ingressoRequest: IngressoRequest)
-            : HttpResponse<IngressoResponse> {
+            : HttpResponse<Any> {
 
         val ingresso = ingressoRequest.ToModel()
         ingressoService.atualizar(id, ingresso)
-        return HttpResponse.ok(IngressoResponse(ingresso))
+        return HttpResponse.ok()
     }
 
     @Delete("/{id}")
     fun deletar(@PathVariable id: String): HttpResponse<Unit> {
         ingressoService.deletar(id)
-        return HttpResponse.noContent()
+        return HttpResponse.ok()
     }
 }
